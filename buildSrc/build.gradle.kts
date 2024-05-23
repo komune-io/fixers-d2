f2ef7168d217a0b0a829e6c336dbd2d09cd42d00
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-05-23 21:59:33 +0200

    Update fixers version to 0.18.0-SNAPSHOT

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    implementation("io.komune.fixers.gradle:dependencies:0.18.0.beta.4")
+    implementation("io.komune.fixers.gradle:dependencies:0.18.0-SNAPSHOT")
 }
