6ce321796ac3e16ca61dcb2417340581e26ef074
Adrien Poupard <adrien@komune.io>
2025-08-09 17:23:27 +0200

    chore(build.gradle.kts): conditionally include mavenLocal repository based on environment variable refactor(Dependencies.kt): format code for better readability and consistency The mavenLocal repository is now included conditionally based on the MAVEN_LOCAL_USE environment variable, allowing for more flexible build configurations. Additionally, the code formatting is improved for better readability and consistency across the file.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -7,30 +7,31 @@ import org.gradle.api.artifacts.dsl.RepositoryHandler
 import org.gradle.kotlin.dsl.embeddedKotlinVersion
 
 object PluginVersions {
-	val fixers = FixersPluginVersions.fixers
-	val fixersOlderVersion = "0.17.0"
-	var kotlinDsl = embeddedKotlinVersion
-	var kotlin = FixersPluginVersions.kotlin
-	var dokka = "1.9.20"
-	const val gradlePublish = "1.2.0"
+    val fixers = FixersPluginVersions.fixers
+    val fixersOlderVersion = "0.17.0"
+    var kotlinDsl = embeddedKotlinVersion
+    var kotlin = FixersPluginVersions.kotlin
+    var dokka = "1.9.20"
+    const val gradlePublish = "1.2.0"
 }
 
 object Versions {
-	const val junit = FixersVersions.Test.junit
-	const val jackson = FixersVersions.Json.jackson
+    const val junit = FixersVersions.Test.junit
+    const val jackson = FixersVersions.Json.jackson
 }
 
 object Dependencies {
-	object Jvm {
-		object Kotlin {
-			fun coroutines(scope: Scope) = FixersDependencies.Jvm.Kotlin.coroutines(scope)
-		}
-	}
+    object Jvm {
+        object Kotlin {
+            fun coroutines(scope: Scope) = FixersDependencies.Jvm.Kotlin.coroutines(scope)
+        }
+    }
 }
 
 fun RepositoryHandler.defaultRepo() {
-	mavenCentral()
-	mavenLocal()
-	maven { url = URI("https://central.sonatype.com/repository/maven-snapshots") }
-	maven { url = URI("https://repo.spring.io/milestone") }
+    mavenCentral()
+    maven { url = URI("https://central.sonatype.com/repository/maven-snapshots") }
+    if(System.getenv("MAVEN_LOCAL_USE") == "true") {
+        mavenLocal()
+    }
 }
