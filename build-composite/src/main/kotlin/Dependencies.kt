8ed4793420e31cba53fba036f451075b2ecf34c7
Adrien Poupard <adrien@komune.io>
2026-01-28 23:43:13 +0100

    Bump fixersOlderVersion to 0.26.0 and remove unused version constants

--- build-composite/src/main/kotlin/Dependencies.kt
+++ build-composite/src/main/kotlin/Dependencies.kt
@@ -1,23 +1,13 @@
 import io.komune.fixers.gradle.dependencies.FixersDependencies
 import io.komune.fixers.gradle.dependencies.FixersPluginVersions
-import io.komune.fixers.gradle.dependencies.FixersVersions
 import io.komune.fixers.gradle.dependencies.Scope
 import java.net.URI
 import org.gradle.api.artifacts.dsl.RepositoryHandler
-import org.gradle.kotlin.dsl.embeddedKotlinVersion
 
 object PluginVersions {
     val fixers = FixersPluginVersions.fixers
-    val fixersOlderVersion = "0.17.0"
-    var kotlinDsl = embeddedKotlinVersion
+    val fixersOlderVersion = "0.26.0"
     var kotlin = FixersPluginVersions.kotlin
-    var dokka = "1.9.20"
-    const val gradlePublish = "1.2.0"
-}
-
-object Versions {
-    const val junit = FixersVersions.Test.junit
-    const val jackson = FixersVersions.Json.jackson
 }
 
 object Dependencies {
